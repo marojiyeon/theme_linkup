a5f35f40057e6594fcf10776c1f380c231c694fa
marojiyeon <marojiyeon@gmail.com>
2019-08-05 10:42:08 +0900

    First Commit

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+{
+    # Theme information
+    'name': "Theme Linkup",
+    'description': """
+    """,
+    'category': 'Theme',
+    'version': '1.0',
+    'depends': ['website', 'website_theme_install'],
+
+    # templates
+    'data': [
+        'views/header.xml',
+        'views/footer.xml',
+        'views/assets.xml',
+        #'views/options.xml',
+        #'views/snippets.xml',
+    ],
+
+    # demo pages
+    'demo': [
+        #'demo/pages.xml',
+    ],
+
+    # Your information
+    'author': "link-up",
+    'website': "",
+}
